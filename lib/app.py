acf2fff88f17f251fdb79c6bce5478ffa37e5544
wolterpynaffit <63312655+wolterpynaffit@users.noreply.github.com>
2023-08-19 18:57:48 -0400

    Update app.py

--- lib/app.py
+++ lib/app.py
@@ -2,3 +2,7 @@ print ('Hello World')
 print("Hello world!")
 print("Hello sun!")
 print("Hello sky!")
+
+print("Hello world!", end=" ")
+print("Hello sun!", end="!! ")
+print("Hello sky!", end="!!!\n")
