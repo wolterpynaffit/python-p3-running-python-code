5ba388f81e0b8cade7e1985501a409bcbb340c58
wolterpynaffit <63312655+wolterpynaffit@users.noreply.github.com>
2023-08-20 17:05:14 -0400

    Update app.py

--- lib/app.py
+++ lib/app.py
@@ -1,8 +1,1 @@
-print ('Hello World')
-print("Hello world!")
-print("Hello sun!")
-print("Hello sky!")
-
-print("Hello world!", end=" ")
-print("Hello sun!", end="!! ")
-print("Hello sky!", end="!!!\n")
+print ("Hello World! Pass this test, please.")
